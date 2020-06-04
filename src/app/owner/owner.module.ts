ead89d3043b54a72eb64884f65ceb52d726494da
rvcl3n <d.rudenko.1996@gmail.com>
2020-06-04 14:01:44 +0300

    Added Success Error Modals and Date Picker

--- src/app/owner/owner.module.ts
+++ src/app/owner/owner.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from './../shared/shared.module';
  
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
@@ -8,6 +9,7 @@ import { OwnerDetailsComponent } from './owner-details/owner-details.component';
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild([
       { path: 'list', component: OwnerListComponent },
       { path: 'details/:id', component: OwnerDetailsComponent }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
+import { SuccessModalComponent } from './modals/success-modal/success-modal.component';
+import { DatepickerDirective } from './directives/datepicker.directive';
+
+
+
+@NgModule({
+  declarations: [ErrorModalComponent, SuccessModalComponent, DatepickerDirective],
+  imports: [
+    CommonModule
+  ],
+  exports: [ErrorModalComponent, SuccessModalComponent]
+})
+export class SharedModule { }
